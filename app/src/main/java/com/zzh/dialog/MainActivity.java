17438d2f313f3ad32e42d9b9fbc5fc981c3ab553
墨宇 <zzh_hz@126.com>
2019-07-29 14:55:11 +0800

    demo

--- app/src/main/java/com/zzh/dialog/MainActivity.java
+++ app/src/main/java/com/zzh/dialog/MainActivity.java
@@ -1,7 +1,12 @@
 package com.zzh.dialog;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+
+import com.zzh.lib.dialog.Dialoger;
+import com.zzh.lib.dialog.impl.HDialogConfirmView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickView(View v) {
+        HDialogConfirmView view = new HDialogConfirmView(this);
+        view.setTextConfirm("确定");
+        view.setTextCancel("取消");
+        view.setTextContent("ASAAAAAAAAAAAAAAAAAAAA");
+        Dialoger dialoger = view.getDialoger();
+        dialoger.setGravity(Gravity.BOTTOM);
+        dialoger.show();
+
+    }
+
 }
